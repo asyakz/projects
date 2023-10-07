2d57ace395875b63eb34e808affc40dc93cf4707
anastasiia_kazantseva <asyakz@gmail.com>
2023-10-07 22:55:53 +0500

    add Test-yandex-kraud

---
+++ Test-yandex-kraud/js/swiper.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.swiper', {
+
+  // Optional parameters
+  loop: false,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
+});
