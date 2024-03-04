deda03f445cd94497992b872dc65065fb3f13670
anastasiia_kazantseva <asyakz@gmail.com>
2024-03-05 00:38:02 +0500

    add test of ridero

---
+++ Ridero/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import BooksList from './app/components/booksList';
+
+function App() {
+  return (
+    <>
+      <h1>Список книг</h1>
+      <BooksList />
+    </>
+  );
+}
+
+export default App;
